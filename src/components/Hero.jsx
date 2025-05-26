e1153081652c16da36928b5a637a71fd01c00f3e
Darien <sangluo2015@gmail.com>
2025-05-26 02:36:13 -0600

    Renamed profile image for GitHub Pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <section id="home" className="relative min-h-screen flex flex-col items-center justify-center text-center px-4 text-white animated-bg">
       <img
-        src="/profile.jpg"
+        src="/sang-profile.jpg"
         alt="Sang"
         className="w-32 h-32 rounded-full mb-6 border-4 border-blue-500 shadow-lg"
         />
